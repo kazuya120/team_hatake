1ed007a5b11cc81dc17540b92309562f1f1c9b23
kazuya120 <songlinheye2@gmail.com>
2021-12-25 06:19:16 +0000

    finish

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -10,6 +10,7 @@ class Blog < ApplicationRecord
   def favorited_by?(user)
     favorites.where(user_id: user.id).exists?
   end
+
   validates :title, presence: true
 	validates :body, presence: true, length: {maximum: 200}
 end
